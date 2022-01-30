c658e81c7620a02e93d5d307475881dcdd1ba8a3
AzCraft <47869198+AstralShadow@users.noreply.github.com>
2022-01-30 19:34:10 +0200

    [fix] Fixed my memory leaks from Action class

--- include/Actions.hpp
+++ include/Actions.hpp
@@ -46,12 +46,35 @@ struct Command
 class Action
 {
 public: 
-    Action(){}
-    ~Action(){}
+    Action(Speech data) :
+        _data(data), _state(action_speech) { }
+
+    Action(Action const& action) :
+        _data(), _state(action._state)
+    {
+        copy_union_val(action._data);
+    }
+
+    void operator =(Action const& action)
+    {
+        _state = action._state;
+        clear_union_val();
+        copy_union_val(action._data);
+    }
+
+    ~Action()
+    {
+        clear_union_val();
+    }
 
 private:
     union ActionData{
         ActionData(){}
+        ActionData(Speech data) : speech(data) {}
+        ActionData(Choice data) : choice(data) {}
+        ActionData(Flag data) : flag(data) {}
+        ActionData(Condition data) : condition(data) {}
+        ActionData(Command data) : command(data) {}
         ~ActionData(){}
 
         Speech speech;
@@ -68,6 +91,50 @@ private:
         action_condition,
         action_command
     } _state;
+
+    void clear_union_val()
+    {
+        switch(_state)
+        {
+            case action_speech:
+                _data.speech.~Speech();
+                break;
+            case action_choice:
+                _data.choice.~Choice();
+                break;
+            case action_flag:
+                _data.flag.~string();
+                break;
+            case action_condition:
+                _data.condition.~Condition();
+                break;
+            case action_command:
+                _data.command.~Command();
+                break;
+        }
+    }
+
+    void copy_union_val(ActionData const& data)
+    {
+        switch(_state)
+        {
+            case action_speech:
+                _data.speech = data.speech;
+                break;
+            case action_choice:
+                _data.choice = data.choice;
+                break;
+            case action_flag:
+                _data.flag = data.flag;
+                break;
+            case action_condition:
+                _data.condition = data.condition;
+                break;
+            case action_command:
+                _data.command = data.command;
+                break;
+        }
+    }
 };
 
 
